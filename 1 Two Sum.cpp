a825d5af41f9475cea67a53853f8a25beaf2cfc4
Darren <iroot900@gmail.com>
2015-06-09 16:31:33 -0500

    Update 1 Two Sum.cpp

--- 1 Two Sum.cpp
+++ 1 Two Sum.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> data;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            data[nums[i]]=i+1;
+        }
+        vector<int> result;
+        for(int i=0;i<n;i++)
+        {
+            auto it=data.find(target-nums[i]);
+            if(it!=data.end())
+            {
+                if(i+1==it->second) continue;// must be two numbers in different position
+                result.push_back(i+1);
+                result.push_back(it->second);
+                return result;
+            }
+        }
+    }
+};
