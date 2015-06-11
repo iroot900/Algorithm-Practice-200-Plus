3bcaed02f7e1d032d6d47634e8e0c04aede24553
Darren <iroot900@gmail.com>
2015-06-10 23:15:27 -0500

    Create 027 Remove Element.cpp

---
+++ 027 Remove Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int p=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==val) continue;
+            nums[p++]=nums[i];
+        }
+        return p;
+    }
+};.cpp
