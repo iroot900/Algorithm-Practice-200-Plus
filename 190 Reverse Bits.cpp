5104ae7d70e3603a36020e9925bdd5c623cb6595
Darren <iroot900@gmail.com>
2015-06-16 12:06:23 -0500

    Create 190 Reverse Bits.cpp

---
+++ 190 Reverse Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result;
+        for(int i=0;i<32;++i)
+        {
+            result+=((n&1)<<(31-i));
+            n>>=1;
+        }
+        return result;
+    }
+};
