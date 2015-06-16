9f19f553031b185229adc3cb1430748f4364beed
Darren <iroot900@gmail.com>
2015-06-16 12:03:22 -0500

    Create 172 Factorial Trailing Zeroes.cpp

---
+++ 172 Factorial Trailing Zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int num=0;
+        while(n)
+        {
+            num+=n/5;
+            n/=5;
+        }
+        return num;
+    }
+};
