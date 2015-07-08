f531e00ccfdfff82c0b48ecf59a989906bdefbe2
Darren <iroot900@gmail.com>
2015-07-08 12:45:24 -0500

    Update 218 The Skyline Problem.cpp

--- 218 The Skyline Problem.cpp
+++ 218 The Skyline Problem.cpp
@@ -1,117 +1,75 @@
 class Solution {
 public:
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<pair<int,int>> skylines;
-        int n=buildings.size(); 
-        if(n==0) return skylines;
-        return divider(buildings, 0, n-1);
+        // sky line problem 
+        int n=buildings.size();
+        if(n==0) return {vector<pair<int,int>> ()};
+        auto result= Skyline(buildings, 0, n-1);
+        return remove(result);
     }
     
-    vector<pair<int,int>> divider(const vector<vector<int>>& buildings, int left, int right)
+    vector<pair<int,int>> Skyline(vector<vector<int>>& buildings,int left, int right)
     {
-        if(left==right)
+        if(left==right) 
         {
-            vector<pair<int,int>> ret;
-            ret.push_back(make_pair(buildings[left][0],buildings[left][2]));
-            ret.push_back(make_pair(buildings[left][1],0));
-            return ret;
+            return { {buildings[left][0],buildings[left][2]}, {buildings[left][1],0}};
         }
-       
         int mid=(left+right)>>1;
-        auto skyLeft=divider(buildings, left, mid); // there is no "-", so all's good!!!
-        auto skyRight=divider(buildings, mid+1, right);
-        return merge(skyLeft, skyRight);
+        auto leftSky=Skyline( buildings, left,  mid);
+        auto rightSky=Skyline( buildings, mid+1,  right);
+        return merge( leftSky,rightSky );
     }
     
-    vector<pair<int,int>> merge(const vector<pair<int, int>> &skyLeft, const vector<pair<int, int>> &skyRight)
+    vector<pair<int,int>> merge( const vector<pair<int,int>> & leftSky, const vector<pair<int,int>> & rightSky)
     {
-        vector<pair<int, int>> result;
-        int m=skyLeft.size(), n=skyRight.size();
-        int left=0, right=0;
+        int left=leftSky.size();
+        int right=rightSky.size();
+        int i=0,j=0;
         
+        vector<pair<int,int>> result;
         int leftH=0, rightH=0;
-        while(left<m||right<n)
+        while(i<left&&j<right)
         {
-            if(left<m&&right<n)
+            if(leftSky[i].first<rightSky[j].first)
             {
-                if(skyLeft[left].first<skyRight[right].first)
-                {
-                    auto P=skyLeft[left].first;
-                    auto H=max(skyLeft[left].second,rightH);
-                    // outside logic --- update P and H. 
-                    // inside logic. --- H compare P compare. 
-                    // don't combine
-                    if(result.size()>0)
-                    {
-                        if(result.back().second==H) 
-                        { }
-                        else if(result.back().first==P)
-                           result.back().second=H;// not again. max 
-                        else 
-                           result.push_back(make_pair(P,H));
-                    }
-                    else
-                    result.push_back(make_pair(P,H));
-                    leftH=skyLeft[left].second;
-                    ++left;
-                }
-                else
-                {
-                    auto P=skyRight[right].first;
-                    auto H=max(skyRight[right].second,leftH);
-                    if(result.size()>0)
-                    {
-                        if(result.back().second==H) 
-                        { }
-                        else if(result.back().first==P)
-                           result.back().second=H;
-                        else   
-                           result.push_back(make_pair(P,H));
-                    }
-                    else
-                    result.push_back(make_pair(P,H));
-                    rightH=skyRight[right].second;
-                    ++right;
-                }
+                leftH=leftSky[i].second;
+                result.push_back(make_pair( leftSky[i].first,max(leftH,rightH)  ));
+                ++i;
             }
-            else if(left<m)
+            else if(leftSky[i].first>rightSky[j].first)
             {
-                    auto P=skyLeft[left].first;
-                    auto H=max(skyLeft[left].second,rightH);
-                    if(result.size()>0)
-                    {
-                        if(result.back().second==H) 
-                        { }
-                        else if(result.back().first==P)
-                           result.back().second=H;
-                        else  
-                           result.push_back(make_pair(P,H));
-                    }
-                    else
-                    result.push_back(make_pair(P,H));
-                    leftH=skyLeft[left].second;
-                    ++left;
+                rightH=rightSky[j].second;
+                result.push_back(make_pair( rightSky[j].first,max(leftH,rightH)  ));
+                ++j;
             }
             else 
             {
-                    auto P=skyRight[right].first;
-                    auto H=max(skyRight[right].second,leftH);
-                    if(result.size()>0)
-                    {
-                        if(result.back().second==H) 
-                        { }
-                        else if(result.back().first==P)
-                           result.back().second=H;
-                        else  
-                           result.push_back(make_pair(P,H));
-                    }
-                    else
-                    result.push_back(make_pair(P,H));
-                    rightH=skyRight[right].second;
-                    ++right;
+                leftH=leftSky[i].second;
+                rightH=rightSky[j].second;
+                result.push_back(make_pair( rightSky[j].first,max(leftH,rightH)  ));
+                ++i;++j;
             }
         }
-        // unique it..
+        
+        while(i<left) { result.push_back(make_pair( leftSky[i].first,leftSky[i].second)); ++i; }
+        while(j<right) { result.push_back(make_pair( rightSky[j].first,rightSky[j].second )); ++j; }
+        
         return result;
     }
-};
+    
+    vector<pair<int,int>>  remove(vector<pair<int,int>> & result)
+    {
+        vector<pair<int,int>> ret;
+        for(auto line:result)
+        {
+            if(ret.empty())
+            {
+                ret.push_back(line);
+            }
+            else if(line.second!=ret.back().second)
+            {
+                ret.push_back(line);
+            }
+        }
+        return ret;
+    }
