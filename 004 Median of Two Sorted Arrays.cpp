99f0bf7e077b113ee714840ce3ca86a0e8750742
Darren <iroot900@gmail.com>
2015-07-08 00:49:02 -0500

    Update 004 Median of Two Sorted Arrays.cpp

--- 004 Median of Two Sorted Arrays.cpp
+++ 004 Median of Two Sorted Arrays.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    //I'm not happy with this solution. too easy to make a mistake. 
-    //The argument use to be array[] which make things way more easy to handle by passing array address
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(),n=nums2.size();
-        int len=m+n;                   
-        if(len&1) return findK(nums1,nums2,len/2+1,0,0);
-        else  return (findK(nums1,nums2,len/2,0,0)+findK(nums1,nums2,len/2+1,0,0))/2;
-    }
-    
-      double findK(vector<int>& nums1, vector<int>& nums2, int k,int left, int right)
-    {   //  1   2    4 , 3
-        //main logic and boundary condition
-        if(left==(int)nums1.size()) return nums2[right+k-1];
-        if(right==(int)nums2.size()) return nums1[left+k-1];
+        // find median of two sorted array.  /
+        int m=nums1.size();
+        int n=nums2.size();
+        int total=m+n;
+        if(total&1)  
+        return findKth(nums1, 0, nums2,  0,  (m+n)/2+1);
+        else 
+        return (findKth(nums1,  0, nums2,  0,  (m+n)/2+1)+findKth(nums1,  0, nums2,  0,  (m+n)/2))/(2.0); 
+    } 
+    int findKth(vector<int>& nums1, int left, vector<int>& nums2, int right, int k)
+    {
         
-        if(k==1) return min(nums1[left],nums2[right]);  
-        int ln=k/2; //1   
-        if(ln>(int)nums1.size()-left)
+        // boundary one
+        if(nums1.size()==left) return nums2[right+k-1];
+        if(nums2.size()==right) return nums1[left+k-1];
+        // boundary two 
+        if(k==1)    return min(nums1[left],nums2[right]);
+        
+        //  one side could be gone so go middle or the small side. k==2 k==3 k==4 
+        int len=k/2;
+        
+        // left as a bench
+        if(len>nums1.size()-left)
         {
-            if(nums1.back()<nums2[right+k-(nums1.size()-left)-1])
-            return findK(nums1,nums2,k-((int)nums1.size()-left),nums1.size(),right);
-            else
-            return findK(nums1,nums2,nums1.size()-left,left,right+k-(nums1.size()-left));
+            len=nums1.size()-left; // left side is too small. take all left. 
+        }
+        else if(k-len>nums2.size()-right) // right side is too small. 
+        {
+            len=(k-(nums2.size()-right)) ;
         }
         
-        int rn=k-k/2; //1
-        if(rn>(int)nums2.size()-right) 
-        {
-            if(nums2.back()<nums1[left+k-(nums2.size()-right)-1])
-            return findK(nums1,nums2,k-((int)nums2.size()-right),left,nums2.size());
-            else return findK(nums1,nums2,nums2.size()-right,left+k-(nums2.size()-right),right);
-        }
-        //2   0 ,  1        0+2-1   [1]
-        if(nums1[left+ln-1]==nums2[right+rn-1]) return nums1[left+ln-1];
-        else if(nums1[left+ln-1]>nums2[right+rn-1]) return findK(nums1,nums2,k-rn,left,right+rn);
-        if(nums1[left+ln-1]<nums2[right+rn-1]) return findK(nums1,nums2,k-ln,left+ln,right);
+        if(nums1[left+len-1]==nums2[right+(k-len)-1]) return nums1[left+len-1];
+        else if (nums1[left+len-1]>nums2[right+(k-len)-1]) return findKth(nums1,  left, nums2,  right+(k-len),  k-(k-len));// keep left
+        else return findKth(nums1,  left+len, nums2,  right,  k-len);
     }
 };
