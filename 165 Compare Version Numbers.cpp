4cda4c76cfa9ae0977b27f4803d9c4ba82477327
Darren <iroot900@gmail.com>
2015-07-10 10:58:48 -0500

    Update 165 Compare Version Numbers.cpp

--- 165 Compare Version Numbers.cpp
+++ 165 Compare Version Numbers.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        int startL=0, startR=0;
-        int m=version1.size();
-        int n=version2.size();
-        string left, right;
-        
-        // if there are ways to do in human mind, it could be done in logic 
-        while(startL!=m||startR!=n)
+        size_t startA=0, startB=0;
+        // main logic, one side is over then it's over,   difference, then it's over. 
+        // 
+        while(1)
         {
-            int i=0;
-            for(i=startL;i<m;++i)
-            {
-                if(version1[i]=='.')
-                {
-                    left=version1.substr(startL,i-startL);
-                    startL=i+1; break;
-                }
-            }
-            if(i==m&&version1[m-1]!='.')
-            {
-                left=version1.substr(startL,m-startL);
-                startL=m;
-            }
-            for(i=startR;i<n;++i)
-            {
-                if(version2[i]=='.')
-                {
-                    right=version2.substr(startR,i-startR);
-                    startR=i+1; break;
-                }
-            }
-            if(i==n&&version2[n-1]!='.')
-            {
-                right=version2.substr(startR,n-startR);
-                startR=n;
-            }
-            while(left.size()>0&&left[0]=='0') left=left.substr(1);
-            while(right.size()>0&&right[0]=='0') right=right.substr(1);
-            if(left==""&&right==""&&startL==m&&startR==n) return 0;
-            if(left==""&&right=="") continue;
-            else if(left=="") return -1;
-            else if(right=="") return 1;
-            if(stoi(left)>stoi(right)) return 1;
-            else if(stoi(left)<stoi(right)) return -1;
+            string left, right;
+            auto p=version1.find_first_of(".",startA);
+            if(p!=string::npos) {left=version1.substr(startA,p-startA); startA=p+1;}
+            else if(startA!=version1.size()) {left=version1.substr(startA);startA=version1.size();}
+            else {left="0";}
+            
+            auto pp=version2.find_first_of(".",startB);
+            if(pp!=string::npos) {right=version2.substr(startB,pp-startB); startB=pp+1;}
+            else if(startB!=version2.size()) {right=version2.substr(startB);startB=version2.size();}
+            else {right="0";}
+            
+            if(stoi(left)<stoi(right)) return -1;
+            if(stoi(right)<stoi(left)) return 1;
+            if(p==string::npos&&pp==string::npos) return 0;
         }
         return 0;
     }
