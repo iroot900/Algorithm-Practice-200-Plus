246f3f50a78bc478666b375159ba261d30943b8f
Darren <iroot900@gmail.com>
2015-07-11 15:55:17 -0500

    Update 213 House Robber II.cpp

--- 213 House Robber II.cpp
+++ 213 House Robber II.cpp
@@ -3,31 +3,24 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0) return 0;
-        if(n<3) return *max_element(begin(nums),end(nums));
-        vector<int> f(n+1);
-        f[1]=nums[0];
-        f[2]=max(nums[0],nums[1]);
-        // rub the first one, and not the last;
-        // rub the second, and the last;
-        for(int i=3;i<=n-1;++i)
+        if(n==1) return nums[0];
+        vector<int> f(n);
+        f[0]=nums[0];
+        f[1]=max(nums[0],nums[1]);
+        if(n==2) return f[1];
+        //start with the first house. and end with second last;
+        //or start with the second, and end with last;
+        for(int i=2;i<n-1;++i)
         {
-            // main logic. starting form 1 end n-1 . or 2 and n --->the max, but you'r not realy at 1 or n-1
-            // exactly where to start or stop is determine by dynamics.
-            // but they're in two catergories. 
-            
-            // starting from one, it's same, just end with n-1
-            f[i]=max(f[i-2]+nums[i-1],f[i-1]);
+            f[i]=max(f[i-1],f[i-2]+nums[i]);
         }
-        
-        vector<int> g(n+1);
-        g[1]=0;
-        g[2]=nums[1];
-        for(int i=3;i<=n;++i)
+        int mmax=f[n-2];
+        f[0]=0;
+        f[1]=nums[1];
+        for(int i=2;i<n;++i)
         {
-            //startring from 2. then 1 is zeros. just can't take it!
-            g[i]=max(g[i-2]+nums[i-1],g[i-1]);
+            f[i]=max(f[i-1],f[i-2]+nums[i]);
         }
-        
-        return max(f[n-1],g[n]);
+        return max(f[n-1],mmax);
     }
 };
