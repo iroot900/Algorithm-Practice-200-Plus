3f7ba028d683b6f186b23d1abd2be407e2e249da
Darren <iroot@gmail.com>
2015-07-18 19:30:21 -0500

    add Max length of loop

---
+++ A 008 Maximum Length of the loop.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int maxLoop( vector<int> & nextIndex)
+{
+	int mlen=0;
+	int n=nextIndex.size();
+	vector<bool> visited(n,false);
+	unordered_set<int> previous;
+	for(int i=0;i<n;++i)
+	{
+		int cur=i;
+		cout<<i<<" :";
+		while(1)
+		{
+			cout<<cur<<" ";
+			if(previous.count(cur)!=0) {mlen=max(mlen,(int)previous.size());previous.clear();}
+			if(visited[cur]) {previous.clear(); break;}
+			visited[cur]=true;
+			previous.insert(cur);
+			cur=nextIndex[cur];
+		}
+		cout<<endl;
+	}
+	return mlen;
+}
+
+int main()
+{
+	vector<int> nextIndex={3,2,1,4,0};
+
+	cout<<maxLoop( nextIndex)<<endl;
+}
