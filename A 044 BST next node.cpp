149761a799778920a07b61b1b7fc8c42c49b01b8
Darren <iroot@gmail.com>
2015-07-21 14:22:47 -0500

    BST search iteration

--- A 044 BST next node.cpp
+++ A 044 BST next node.cpp
@@ -34,17 +34,34 @@ void next(node* root, int val, int& nxt)
 	}
 }
 
+bool find(node* root,int val)
+{
+	auto cur=root;
+	while(cur)
+	{
+		if(cur->val==val) return true;
+		else if(cur->val>val) cur=cur->left;
+		else cur=cur->right;
+	}
+	return false;
+}
+
 int main()
 {
 	node* root=nullptr;
 	for(int i=0;i<10;++i)
-		add(root,i);
+		add(root,i*2);
 	travel(root);
 	cout<<endl;
 	for(int i=0;i<9;++i)
 	{
 		int nxt=INT_MAX;
-		next(root,i, nxt);
-		cout<<i<<"-->"<<nxt<<endl;
+		next(root,i*2, nxt);
+		cout<<i*2<<"-next->"<<(nxt==INT_MAX?INT_MIN:nxt)<<endl;
 	}
+
+	for(int i=0;i<5;++i)
+		cout<<"find->" <<i*2<<" : "<<find(root,i*2)<<endl;
+	for(int i=0;i<5;++i)
+		cout<<"find->" <<i*2+1<<" : "<<find(root,i*2+1)<<endl;
 }
