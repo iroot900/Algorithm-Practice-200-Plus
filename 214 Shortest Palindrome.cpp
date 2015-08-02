26401904b281b1a3c6a374e4147f0f21f631e0d5
Darren <iroot900@gmail.com>
2015-08-02 14:47:30 -0500

    Update 214 Shortest Palindrome.cpp

--- 214 Shortest Palindrome.cpp
+++ 214 Shortest Palindrome.cpp
@@ -3,22 +3,26 @@ public:
     string shortestPalindrome(string s) {
         int n=s.size();
         if(n<2) return s;
-        string temp=s+string(s.rbegin(),s.rend());
-        int m=temp.size();
-        vector<int> prefix(m);
-        prefix[0]=0;
-        int last=0;
-        for(int i=1;i<m;++i)
+        //     aaacd   dcaaa
+        string rstr=string(s.rbegin(),s.rend());
+        string str=s+rstr;
+        auto pre=prefix(str);
+        int len=n-pre[2*n-1];
+        return rstr.substr(0,len)+s; 
+    }
+    
+    vector<int> prefix(string str)
+    {
+        int n=str.size();
+        vector<int> pre(n,0);
+        pre[0]=0;
+        
+        for(int i=1;i<n;++i)
         {
-            while(last>0&&temp[i]!=temp[last])
-            {
-                last=prefix[last-1];
-            }
-            if(temp[last]==temp[i])
-            last++;
-            prefix[i]=last;
+            int last=pre[i-1];
+            while(last>0&&(str[i]!=str[last]||last>=n/2)) { last=pre[last-1]; }
+            if(str[i]==str[last]) pre[i]=last+1;
         }
-        int len=n-prefix[m-1];
-        return string(s.rbegin(),s.rbegin()+len)+s;
+        return pre;
     }
 };
