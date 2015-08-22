83df90d235592a2e6e9aa75e412f548b043de514
Darren <iroot900@gmail.com>
2015-08-22 15:13:24 -0500

    Update 191 Number of 1 Bits.cpp

--- 191 Number of 1 Bits.cpp
+++ 191 Number of 1 Bits.cpp
@@ -8,5 +8,13 @@ public:
             n>>=1;
         }
         return sum;
+        
+        /* more efficient way
+        int count=0;   
+        while(n) {n=n&(n-1); ++count; }
+        return count;
+        */
     }
+    
+    
 };
